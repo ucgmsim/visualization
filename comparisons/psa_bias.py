765129728cc97ee8d7b373972fe13a3ef5b5e6e7
ttgenius <melodyzyz@gmail.com>
2018-07-26 16:55:58 +1200

    merged funcs in psa_bias

--- comparisons/psa_bias.py
+++ comparisons/psa_bias.py
@@ -15,6 +15,7 @@ np_startswith = np.core.defchararray.startswith
 np_endswith = np.core.defchararray.endswith
 np_lstrip = np.core.defchararray.lstrip
 
+
 def load_args():
     """
     Process command line arguments.
@@ -42,70 +43,48 @@ def load_args():
 
     return args
 
+
+def get_ims_psa(arg_sim):
+    sim_ims = load_im_file(arg_sim, all_psa=True)
+    sim_ims = sim_ims[sim_ims.component == args.comp]
+    sim_psa = [sim_ims.dtype.names[col_i] for col_i in np.where(np.logical_and(np_startswith(sim_ims.dtype.names, 'pSA_'), np.logical_not(np_endswith(sim_ims.dtype.names, 'sigma'))))[0]]
+    return sim_ims, sim_psa
+
+
+def calc_ratio(arg_obs, arg_sim):
+    sim_ims, sim_psa = get_ims_psa(arg_sim)
+    obs_ims, obs_psa = get_ims_psa(arg_obs)
+
+    psa_names = np.intersect1d(obs_psa, sim_psa)
+    psa_vals = np_lstrip(psa_names, chars='pSA_').astype(np.float32)
+    # sorted
+    sort_idx = np.argsort(psa_vals)
+    psa_names = psa_names[sort_idx]
+    psa_vals = psa_vals[sort_idx]
+    del sim_psa, sort_idx
+
+    # common stations
+    obs_in_sim = argsearch(obs_ims.station, sim_ims.station)
+    obs_idx = np.where(obs_in_sim.mask == False)[0]
+    sim_idx = obs_in_sim.compressed()
+    del obs_in_sim
+
+    # plotting data
+    psa_ratios = np.log(obs_ims[psa_names][obs_idx].tolist()) - \
+                 np.log(sim_ims[psa_names][sim_idx].tolist())
+    psa_means = np.mean(psa_ratios, axis=0)
+    psa_std = np.std(psa_ratios, axis=0)
+    del sim_ims, obs_idx, sim_idx, psa_names, psa_ratios
+    return psa_vals, psa_means, psa_std
+
 ###
 ### MAIN
 ###
 
 args = load_args()
 
-# load im files (slow) for component, available pSA columns
-sim_ims = load_im_file(args.sim, all_psa = True)
-sim_ims = sim_ims[sim_ims.component == args.comp]
-sim_psa = [sim_ims.dtype.names[col_i] for col_i in \
-           np.where(np_startswith(sim_ims.dtype.names, 'pSA_'))[0]]
-obs_ims = load_im_file(args.obs, all_psa = True)
-obs_ims = obs_ims[obs_ims.component == args.comp]
-obs_psa = [obs_ims.dtype.names[col_i] for col_i in \
-           np.where(np_startswith(obs_ims.dtype.names, 'pSA_'))[0]]
-emp_ims = load_im_file(args.emp, all_psa = True)
-emp_ims = emp_ims[emp_ims.component == args.comp]
-emp_psa = [emp_ims.dtype.names[col_i] for col_i in \
-           np.where(np.logical_and(np_startswith(emp_ims.dtype.names, 'pSA_'), np.logical_not(np_endswith(emp_ims.dtype.names, 'sigma'))))[0]]
-print(emp_psa)
-# common pSAs
-psa_names = np.intersect1d(obs_psa, sim_psa)
-psa_vals = np_lstrip(psa_names, chars='pSA_').astype(np.float32)
-# sorted
-sort_idx = np.argsort(psa_vals)
-psa_names = psa_names[sort_idx]
-psa_vals = psa_vals[sort_idx]
-del sim_psa, sort_idx
-
-# common stations
-obs_in_sim = argsearch(obs_ims.station, sim_ims.station)
-obs_idx = np.where(obs_in_sim.mask == False)[0]
-sim_idx = obs_in_sim.compressed()
-del obs_in_sim
-
-# plotting data
-psa_ratios = np.log(obs_ims[psa_names][obs_idx].tolist()) - \
-             np.log(sim_ims[psa_names][sim_idx].tolist())
-psa_means = np.mean(psa_ratios, axis = 0)
-psa_std = np.std(psa_ratios, axis = 0)
-del sim_ims, obs_idx, sim_idx, psa_names, psa_ratios
-
-#TODO write a wrapp fuction to merge with the obs-sim
-psa_names_emp = np.intersect1d(obs_psa, emp_psa)
-psa_vals_emp = np_lstrip(psa_names_emp, chars='pSA_').astype(np.float32)
-# sorted
-sort_idx_emp = np.argsort(psa_vals_emp)
-psa_names_emp = psa_names_emp[sort_idx_emp]
-psa_vals_emp = psa_vals_emp[sort_idx_emp]
-del obs_psa, emp_psa, sort_idx_emp
-
-
-obs_in_emp = argsearch(obs_ims.station, emp_ims.station)
-obs_idx = np.where(obs_in_emp.mask == False)[0]
-emp_idx = obs_in_emp.compressed()
-del obs_in_emp
-
-# plotting data
-psa_ratios_emp = np.log(obs_ims[psa_names_emp][obs_idx].tolist()) - \
-             np.log(emp_ims[psa_names_emp][emp_idx].tolist())
-psa_means_emp = np.mean(psa_ratios_emp, axis = 0)
-psa_std_emp = np.std(psa_ratios_emp, axis = 0)
-del obs_ims, emp_ims, obs_idx, emp_idx, psa_names_emp, psa_ratios_emp
-
+psa_vals, psa_means, psa_std = calc_ratio(args.obs, args.sim)
+psa_vals_emp, psa_means_emp, psa_std_emp = calc_ratio(args.obs, args.emp)
 
 # plot
 fig = plt.figure(figsize = (14, 7.5), dpi = 100)
