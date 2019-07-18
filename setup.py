13f8575233dcf4d0ae0b950f9a7b8cfa758ea165
ttgenius <melodyzyz@gmail.com>
2019-07-18 14:23:12 +1200

    moved scripts locations for packaging

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ Install using pip, e.g. pip install ./visualization
 use --no-deps to prevent re-installation of dependencies
 use -I to force re-install
 """
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name="visualization",
     version="1.0.0",
-    packages=find_packages(),
+    packages=['visualization'],
     url="https://github.com/ucgmsim/visualization",
     description="visualization code",
     install_requires=["numpy>=1.14.3"],
