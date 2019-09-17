eee9bbaf90cc8f0775c1142a7bd28cee07f65bed
Viktor Polak <viktor.polak@canterbury.ac.nz>
2019-09-17 15:30:09 +1200

    functional script

--- visualization/gmt/plot_items.py
+++ visualization/gmt/plot_items.py
@@ -11,6 +11,7 @@ add vm corners created with srfinfo2vm:
 for more information on parameters, use -h
 """
 from argparse import ArgumentParser
+from functools import partial
 from glob import glob
 from multiprocessing import Pool
 import os
@@ -71,9 +72,14 @@ def get_args():
         action="append",
         help="xyts.e3d to plot outlines for, use wildcards, repeat as needed")
     arg("--xyz", help="path to file containing lon, lat, value_1 .. value_N")
+    arg("--xyz-landmask", help="only show overlay over land", action="store_true")
     arg("--xyz-size", help="size of points or grid spacing eg: 1c or 1k")
+    arg("--xyz-shape", help="shape of points eg: t,c,s...", default="t")
     arg("--xyz-cpt", help="CPT to use for overlay data", default="hot")
     arg("--xyz-cpt-invert", help="inverts CPT", action="store_true")
+    arg("--xyz-cpt-categorical", help="colour scale as discreet values", action="store_true")
+    arg("--xyz-cpt-intervals", help="if categorical, display value intervals", action="store_true")
+    arg("--xyz-cpt-label", help="colour scale label", default="values")
     arg("--xyz-cpt-min", help="CPT minimum value")
     arg("--xyz-cpt-max", help="CPT maximum value")
     arg("--xyz-cpt-inc", help="CPT colour increments")
@@ -162,23 +168,15 @@ def load_xyz(args):
     else:
         xyz_info["dxy"] = args.xyz_size
     # these corners are without projection, will hold all points, should be cropped
+
     corners = [[x_min, y_min], [x_max, y_min], [x_max, y_max], [x_min, y_max]]
-    #gmt.grd_mask(
-    #    "%s/sim.modelpath" % (gmt_temp),
-    #    "%s/mask.grd" % (gmt_temp),
-    #    dx=xyz_info["dxy"],
-    #    dy=xyz_info["dxy"],
-    #    region=xyz_info["region"],
-    #)
     return xyz_info
 
 
 def load_xyz_col(args_info_i):
     args, xyz_info, i = args_info_i
-    swd = os.path.join(gmt_temp, "_%d_" % (i))
-    copy("%s/gmt.conf" % (gmt_temp), swd)
-    copy("%s/gmt.history" % (gmt_temp), swd)
-    
+    swd = os.path.join(gmt_temp, "_xyz%d" % (i))
+    os.makedirs(swd)    
 
     xyz_val = np.loadtxt(args.xyz, usecols=(0, 1, i + 2), dtype="f")
     # prepare cpt
@@ -199,7 +197,7 @@ def load_xyz_col(args_info_i):
         cpt_min = 0
     cpt_inc = (cpt_max / 10.0)
     cpt_tick = (cpt_inc * 2.0)
-    col_cpt = "%s/stations_%d.cpt" % (swd, i)
+    col_cpt = os.path.join(swd, "cpt.cpt")
     # overlay colour scale
     gmt.makecpt(
         args.xyz_cpt,
@@ -210,7 +208,9 @@ def load_xyz_col(args_info_i):
         invert=args.xyz_cpt_invert,
         wd=swd,
     )
-    
+
+    return {"min":cpt_min, "max":cpt_max, "inc":cpt_inc, "tick":cpt_tick}
+
 
 def find_srfs(args, gmt_temp):
     # find srf
@@ -269,6 +269,188 @@ def load_vm_corners(args):
     )
     return vm_corners
 
+
+def load_sizing(wd):
+    pwd = os.path.join(wd, "_size")
+    ps_file = "%s/size.ps" % (pwd)
+    os.makedirs(pwd)
+
+    map_width = 7
+
+    p = gmt.GMTPlot(ps_file)
+    if args.region is None:
+        # auto region?
+        region = gmt.nz_region
+    else:
+        region = list(map(float, args.region.split("/")))
+    if region[1] < -90 and region[0] > 90:
+        region[1] += 360
+    p.spacial("M", region, sizing="%si" % (map_width))
+    size = gmt.mapproject(
+        region[1], region[3], wd=pwd, unit="inch"
+    )
+    p.leave()
+
+    page_width = size[0] + 2 + 0.5
+    page_height = size[1] + 2 + 1
+    gmt.gmt_defaults(ps_media="Custom_{}ix{}i".format(page_width, page_height), wd=wd)
+    
+    return {"size":size, "region":region, "page_width":page_width, "page_height":page_height}
+
+
+def basemap(args, sizing, wd):
+    ps_file = "%s/%s.ps" % (wd, args.filename)
+    p = gmt.GMTPlot(ps_file, reset=False)
+    p.spacial("M", sizing["region"], sizing="%si" % (sizing["size"][0]), x_shift=2, y_shift=2)
+    if args.fast:
+        p.basemap(res="f", land="lightgray", topo=None, road=None, highway=None)
+    else:
+        p.basemap(topo_cpt="grey1", land="lightgray")
+    # border tick labels
+    p.ticks(major=2, minor=0.2)
+    # QuakeCoRE logo
+    p.image("L", "T", "%s/quakecore-logo.png" % (script_dir), width="3i", pos="rel")
+    # title
+    if args.title is not None:
+        p.text(
+            sum(sizing["region"][:2]) / 2.0,
+            sizing["region"][3],
+            args.title,
+            colour="black",
+            align="CB",
+            size=28,
+            dy=0.2,
+        )
+
+    return p
+
+
+def add_items(args, p, gmt_temp):
+    # plot velocity model corners
+    p.path(vm_corners, is_file=False, close=True, width="0.5p", split="-")
+    # add SRF slip
+    finite_faults = False
+    slip_cpt = "%s/slip.cpt" % (gmt_temp)
+    for i_s in i_srf_data:
+        if i_s is None:
+            continue
+        finite_faults = True
+        for plane in xrange(len(i_s[1][1])):
+            p.overlay(
+                "%s/srf%d_%d_slip.bin" % (gmt_temp, i_s[0], plane),
+                slip_cpt,
+                dx=i_s[1][0][0],
+                dy=i_s[1][0][1],
+                climit=2,
+                crop_grd="%s/srf%d_%d_mask.grd" % (gmt_temp, i_s[0], plane),
+                land_crop=False,
+                transparency=35,
+                custom_region=i_s[1][1][plane],
+            )
+    # add outlines for SRFs with slip
+    for c in glob("%s/srf*.cnrs" % (gmt_temp)):
+        p.fault(
+            c,
+            is_srf=False,
+            hyp_size=0,
+            plane_width=0.2,
+            top_width=0.4,
+            hyp_width=0.2,
+            plane_colour=args.fault_colour,
+            top_colour=args.fault_colour,
+            hyp_colour=args.fault_colour,
+        )
+    # add outlines for SRFs without slip
+    for c in glob("%s/srf*.cnrs-X" % (gmt_temp)):
+        p.fault(
+            c,
+            is_srf=False,
+            hyp_size=0,
+            plane_width=0.2,
+            top_width=0.4,
+            hyp_width=0.2,
+            plane_colour=args.outline_fault_colour,
+            top_colour=args.outline_fault_colour,
+            hyp_colour=args.outline_fault_colour,
+        )
+    # add beach balls
+    for bb in glob("%s/beachball*.bb" % (gmt_temp)):
+        p.beachballs(bb, is_file=True, fmt="a", scale=args.bb_scale)
+    # slip scale
+    if finite_faults:
+        # TODO: do not interfere if there are more scales
+        p.cpt_scale(
+            "C",
+            "B",
+            slip_cpt,
+            pos="rel_out",
+            dy="0.5i",
+            label="Slip (cm)",
+            length=map_width * 0.618,
+        )
+
+
+def render_xyz_col(sizing, xyz_i):
+    i, xyz = xyz_i
+    pwd = os.path.join(gmt_temp, "_xyz{}".format(i))
+    ps_file = os.path.join(pwd, "{}_{}.ps".format(args.filename, i))
+    copy(os.path.join(gmt_temp, args.filename + ".ps"), ps_file)
+    copy(os.path.join(gmt_temp, "gmt.conf"), os.path.join(pwd, "gmt.conf"))
+    copy(os.path.join(gmt_temp, "gmt.history"), os.path.join(pwd, "gmt.history"))
+    p = gmt.GMTPlot(ps_file, append=True, reset=False)
+
+    if args.xyz_landmask:
+        p.clip(path=gmt.LINZ_COAST["150k"], is_file=True)
+    if not args.xyz_grid:
+        if args.xyz_size is None:
+            args.xyz_size = "6p"
+        p.points(
+            args.xyz,
+            shape=args.xyz_shape,
+            size=args.xyz_size,
+            fill=None,
+            line=None,
+            cpt="cpt.cpt",
+            cols="0,1,%d" % (i + 2),
+        )
+    if args.xyz_landmask:
+        p.clip()
+
+    # colour scale
+    if args.xyz_cpt_categorical:
+        p.cpt_scale(
+            "C"
+            "B",
+            "cpt.cpt",
+            pos="rel_out",
+            dy=0.5,
+            length=sizing["size"][0] * 0.8,
+            label=args.xyz_cpt_label,
+            arrow_f=False,
+            arrow_b=False,
+            gap=meta["cpt_gap"],
+            intervals=args.xyz_cpt_intervals,
+            categorical=True,
+        )
+    else:
+        p.cpt_scale(
+            "C",
+            "B",
+            "cpt.cpt",
+            xyz["tick"],
+            xyz["inc"],
+            pos="rel_out",
+            dy=0.5,
+            length=sizing["size"][0] * 0.8,
+            label=args.xyz_cpt_label,
+            arrow_f=xyz["max"] > 0,
+            arrow_b=xyz["min"] < 0,
+        )
+
+    p.finalise()
+    p.png(out_dir=".", dpi=args.dpi, background="white")
+
+
 args = get_args()
 gmt_temp = mkdtemp()
 srf_files, srf_0 = find_srfs(args, gmt_temp)
@@ -281,101 +463,21 @@ i_srf_data = pool.map_async(load_srf, zip(range(srf_0, srf_0 + len(srf_files)), 
 xyts_corners = pool.map_async(load_xyts_corners, xyts_files)
 vm_corners = load_vm_corners(args)
 
+sizing = load_sizing(gmt_temp)
+p = basemap(args, sizing, gmt_temp)
+
 xyz_info = xyz_info.get()
 xyz = pool.map_async(load_xyz_col, zip([args] * xyz_ncol, [xyz_info] * xyz_ncol, range(0, xyz_ncol)))
 
 xyz = xyz.get()
-exit()
 i_srf_data = i_srf_data.get()
 xyts_corners = xyts_corners.get()
 
-
-def basemap(args, wd):
-    ps_file = "%s/%s.ps" % (wd, args.filename)
-    map_width = 9
-    p = gmt.GMTPlot(ps_file)
-    if args.region is None:
-        region = gmt.nz_region
-    else:
-        region = map(float, args.region.split("/"))
-    p.spacial("M", region, sizing="%si" % (map_width), x_shift=2, y_shift=2)
-    if args.fast:
-        p.basemap(res="f", land="lightgray", topo=None, road=None, highway=None)
-    else:
-        p.basemap(topo_cpt="grey1", land="lightgray")
-    # border tick labels
-    p.ticks(major=2, minor=0.2)
-    # QuakeCoRE logo
-    p.image("L", "T", "%s/quakecore-logo.png" % (script_dir), width="3i", pos="rel")
-
-    return p
-
-p = basemap(args, gmt_temp)
-
-# plot velocity model corners
-p.path(vm_corners, is_file=False, close=True, width="0.5p", split="-")
-
-# add SRF slip
-finite_faults = False
-for i_s in i_srf_data:
-    if i_s is None:
-        continue
-    finite_faults = True
-    for plane in xrange(len(i_s[1][1])):
-        p.overlay(
-            "%s/srf%d_%d_slip.bin" % (gmt_temp, i_s[0], plane),
-            slip_cpt,
-            dx=i_s[1][0][0],
-            dy=i_s[1][0][1],
-            climit=2,
-            crop_grd="%s/srf%d_%d_mask.grd" % (gmt_temp, i_s[0], plane),
-            land_crop=False,
-            transparency=35,
-            custom_region=i_s[1][1][plane],
-        )
-# add outlines for SRFs with slip
-for c in glob("%s/srf*.cnrs" % (gmt_temp)):
-    p.fault(
-        c,
-        is_srf=False,
-        hyp_size=0,
-        plane_width=0.2,
-        top_width=0.4,
-        hyp_width=0.2,
-        plane_colour=args.fault_colour,
-        top_colour=args.fault_colour,
-        hyp_colour=args.fault_colour,
-    )
-# add outlines for SRFs without slip
-for c in glob("%s/srf*.cnrs-X" % (gmt_temp)):
-    p.fault(
-        c,
-        is_srf=False,
-        hyp_size=0,
-        plane_width=0.2,
-        top_width=0.4,
-        hyp_width=0.2,
-        plane_colour=args.outline_fault_colour,
-        top_colour=args.outline_fault_colour,
-        hyp_colour=args.outline_fault_colour,
-    )
-# add beach balls
-for bb in glob("%s/beachball*.bb" % (gmt_temp)):
-    p.beachballs(bb, is_file=True, fmt="a", scale=args.bb_scale)
-
-# slip scale
-if finite_faults:
-    p.cpt_scale(
-        "C",
-        "B",
-        slip_cpt,
-        pos="rel_out",
-        dy="0.5i",
-        label="Slip (cm)",
-        length=map_width * 0.618,
-    )
-
-# output
-p.finalise()
-p.png(out_name=os.path.abspath(args.filename), dpi=args.dpi, background="white")
+add_items(args, p, gmt_temp)
+if args.xyz:
+    xyz_pngs = pool.map_async(partial(render_xyz_col, sizing), enumerate(xyz))
+    xyz_pngs = xyz_pngs.get()
+else:
+    p.finalise()
+    p.png(out_dir=".", dpi=args.dpi, background="white")
 rmtree(gmt_temp)
