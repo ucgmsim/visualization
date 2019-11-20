7e81305a72e4bbc9de1518f5a8079e76602e3bba
jasonmotha <25143301+jasonmotha@users.noreply.github.com>
2019-11-20 15:52:40 +1300

    categorical fix for plot_items

--- visualization/gmt/plot_items.py
+++ visualization/gmt/plot_items.py
@@ -577,6 +577,8 @@ def render_xyz_col(sizing, xyz_info, xyz_i):
         else args.xyz_cpt_labels[i],
         arrow_f=False if args.xyz_cpt_categorical else xyz["max"] > 0,
         arrow_b=False if args.xyz_cpt_categorical else xyz["min"] < 0,
+        categorical=True if args.xyz_cpt_categorical else False,
+        intervals=True if args.xyz_cpt_intervals else False,
     )
 
     p.sites(gmt.sites_major)
